511bd452a9277461144236145adc9fbb403e5616
anhvjp9j9i <sangff2003@gmail.com>
2022-11-23 08:26:36 +0700

    first commit

---
+++ slider/src/main/java/com/fpt/library/slidertypes/DefaultSliderView.java
@@ -0,0 +1,23 @@
+package com.fpt.library.slidertypes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.fpt.library.R;
+
+public class DefaultSliderView extends BaseSliderView{
+
+    public DefaultSliderView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public View getView() {
+        View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_default,null);
+        ImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
+        bindEventAndShow(v, target);
+        return v;
+    }
+}
